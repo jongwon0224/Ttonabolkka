7b024a025ae1016eca1b355edb648c6928dc3eac
PCJ-93 <pw2900@naver.com>
2025-02-05 17:28:13 +0900

    서브 메인페이지 레이아웃 작업

--- src/main/java/com/app/controller/subMain/SubMainController.java
+++ src/main/java/com/app/controller/subMain/SubMainController.java
@@ -1,12 +1,42 @@
 package com.app.controller.subMain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
+import com.app.dto.Places;
 
 
 @Controller
 public class SubMainController {
+	
+	@GetMapping("/subMain")
+	public String subMain(Model model) {
+		List<Places> placesList = new ArrayList<Places>();
+		
+		Places attraction1 = new Places();
+		Places attraction2 = new Places();
+		Places attraction3 = new Places();
+				
+		attraction1.setName("첨성대");
+		attraction2.setName("불국사");
+		attraction3.setName("석굴암");
+		
+		attraction1.setDescription("/img/cheomseongdae.webp");
+		attraction2.setDescription("/img/bulguksa.jpg");
+		attraction3.setDescription("/img/seokguram.jpg");
+		
+		placesList.add(attraction1);
+		placesList.add(attraction2);
+		placesList.add(attraction3);
+		
+		model.addAttribute("placesList", placesList);
+		
+		return "subMain/subMain";
+	}
 	
 	
 	
--- src/main/webapp/WEB-INF/views/subMain/subMain.jsp
+++ src/main/webapp/WEB-INF/views/subMain/subMain.jsp
@@ -1,12 +1,38 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>    
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
+
+<link href="/css/subMain.css" rel="stylesheet">
+
+
 <body>
 
+    <div class="nav_bar">네브 바</div>
+
+    <div class="main_container">
+        <div class="category">
+            <div>관광지</div>
+            <div>테마파크</div>
+            <div>핫 플레이스</div>
+        </div>
+        
+        <c:forEach var="places" items="${placesList}">
+            <div class="places_box">
+                <div class="places_img">
+                    <img src="${places.description}" alt="${places.name}이미지">
+                </div>
+                <div class="places_txt">${places.name}</div>
+            </div>
+        </c:forEach>
+    </div>
+        
 </body>
+
+<script src="/js/subMain.js" ></script>
 </html>
