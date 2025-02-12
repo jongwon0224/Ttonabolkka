ad6261fdbc1110a0de12e29a511b96648cb123e0
YooUisun <yoouisun33@naver.com>
2025-02-12 09:19:56 +0900

    db연결,서버 연결 진행중

--- src/main/java/com/app/dto/detail/Places.java
+++ src/main/java/com/app/dto/detail/Places.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 @Data
 public class Places {
-    private int id;
-    private String name;
-    private String description;
-    private String address;
-    private List<String> imageUrls;  // 여러 이미지 URL을 저장할 리스트 필드
+     int id;
+    String name;
+    String description;
+    String address;
+    String imageUrls;  // 여러 이미지 URL을 저장할 리스트 필드
+    String categoryId;
+    
 }
